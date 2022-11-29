2f573bfaf6bfc5f14a9f935d69bbfecde13bfd7c
AcceptableCasualties <91676490+AcceptableCasualties@users.noreply.github.com>
2022-11-29 16:06:53 +0100

    Bug #1.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
-#include <avr/io.h>
+#include <Wire.h>
+#include <HardwareSerial.h>
 
+//#define ARDUINOMODE
+// Uncomment line above to enable Arduino.h & disable main.
 
+#ifdef ARDUINOMODE
+#include <Arduino.h>
+#endif
+
+void setup() {
+    Serial.begin(9600);
+    Serial.flush();
+
+    Wire.begin();
+
+    Serial.println("Test 1.");
+    Serial.flush();
+
+    Wire.beginTransmission(0x52); // Begin transmission to nunchuk.
+    Wire.write(0xF0);
+    Wire.write(0x55);
+    Wire.endTransmission(); // Actually send data.
+
+    Serial.println("Succesfull transmission.");
+    Serial.flush();
+
+} // Init function.
+
+void loop() {};
+
+#ifndef ARDUINOMODE
 int main() {
+    setup();
     return 0;
 }
+#endif
