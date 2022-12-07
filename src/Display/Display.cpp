46313dd8601b4daf950a47f4478dda101db5edd9
sander2324 <43219603+sander2324@users.noreply.github.com>
2022-12-07 12:45:19 +0100

    Remove unneeded comment

--- src/Display/Display.cpp
+++ src/Display/Display.cpp
@@ -66,7 +66,6 @@ void DisplayClass::spi_init() {
 
     SPCR = (
         (1 << SPE) // SPI Enable
-        // | (1 << SPIE) // SPI Interrupt enable
         | (1 << MSTR) // Set SPI Master mode
         | (1 << SPR1) // Spe
         | (1 << SPR0) //    ed
@@ -99,6 +98,8 @@ void DisplayClass::begin() {
     this->init_display_registers();
 
     this->startup();
+    uint8_t args[1] = {0xFF};
+    this->send_command(0x51);
 }
 
 
