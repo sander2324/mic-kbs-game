0f596cf3e27825d398e91f63505560e9911c2276
sander2324 <43219603+sander2324@users.noreply.github.com>
2023-02-03 00:17:09 +0100

    Add SuperCounter

---
+++ src/SuperCounter/SuperCounter.cpp
@@ -0,0 +1,30 @@
+#include "SuperCounter.h"
+
+
+volatile uint32_t supercounter = 0;
+
+
+ISR(TIMER0_COMPA_vect) {
+    supercounter += 1;
+}
+
+
+// Set Timer0 to trigger TIMER0_COMPA interrupt every 1 millisecond
+void init_supercounter() {
+    // Set Timer0 to wave generation mode 2 (CTC)
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);
+    TCCR0B &= ~(1 << WGM02);
+
+    // Set Timer0 prescaler to 64
+    TCCR0B |= (1 << CS00) | (1 << CS01);
+    TCCR0B &= ~(1 << CS02);
+
+    // Set Output Compare Register A
+    OCR0A = TIMER0_COMPA_VAL;
+
+    // Clear Timer0
+    TCNT0 = 0;
+
+    // Timer0 Output Compare Match A (OCR0A) Interrupt Enable
+    TIMSK0 |= (1 << OCIE0A);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 #include "global_vars.h"
 
+#include "SuperCounter/SuperCounter.h"
 #include "Nunchuck/Nunchuck.h"
 #include "Display/Display.h"
 #include "Monster/Monster.h"
@@ -25,41 +26,6 @@
 #endif
 
 
-volatile uint16_t supercounter = 0;
-volatile bool timer0_compa_hit = false;
-
-
-ISR(TIMER0_COMPA_vect) {
-    timer0_compa_hit = true;
-}
-
-
-// Set Timer0 to trigger TIMER0_COMPA interrupt every 1 millisecond
-inline void init_timer0_registers() {
-    // Set Timer0 to wave generation mode 2 (CTC)
-    TCCR0A |= (1 << WGM00) | (1 << WGM01);
-    TCCR0B &= ~(1 << WGM02);
-
-    // Set Timer0 prescaler to 64
-    TCCR0B |= (1 << CS00) | (1 << CS01);
-    TCCR0B &= ~(1 << CS02);
-
-    // Set Output Compare Register A
-    OCR0A = 249;
-
-    // Clear Timer0
-    TCNT0 = 0;
-
-    // Timer0 Output Compare Match A (OCR0A) Interrupt Enable
-    TIMSK0 |= (1 << OCIE0A);
-}
-
-
-inline void init_registers() {
-    init_timer0_registers();
-}
-
-
 inline void init_twi() {
     Wire.begin();
 }
@@ -69,7 +35,7 @@ void initialize() {
 #if NUNCHUCK_DEBUG
     Serial.begin(9600);
 #endif
-    init_registers();
+    init_supercounter();
     init_twi();
 
     sei();
@@ -115,15 +81,8 @@ int main() {
 
     Battle current_battle = Battle(user, opponent);
     while (true) {
-
-        if (timer0_compa_hit) {
-            timer0_compa_hit = false;
-            supercounter += 1;
-
-
-        }
         current_battle.loop();
-        
+
         // Temporary insert for change brightness.
         PotMeter.checkPotmeterBrightness();
         // Remove the lines above once main is reformatted to have a solid loop part.
