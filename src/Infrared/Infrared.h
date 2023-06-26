b117a7d00b40fb589e644c36752414db43629ad4
sander2324 <43219603+sander2324@users.noreply.github.com>
2023-06-26 10:19:14 +0200

    Try to refactor infrared

--- src/Infrared/Infrared.h
+++ src/Infrared/Infrared.h
@@ -3,21 +3,12 @@
 
 #include <stdint.h>
 
-void init_ir();
-void send_ir(uint16_t input_data); // main function
-void send_start_bit();
-void send_stop_bit();
-void send_bit();
-
-uint16_t receive_ir();
-void receive_bit();
-bool is_bit(uint16_t bit_length); // returns true if received bit equals given bit length
-
-void ir_setup(int host);
-// internal functions
-void pin_init();
-void timer0_init();
-void timer2_init();
-void timer2_clear();
+class Infrared {
+    public:
+        Infrared(uint8_t rxfreq, uint8_t txfreq);
+    private:
+        uint8_t rxfreq;
+        uint8_t txfreq;
+};
 
 #endif
